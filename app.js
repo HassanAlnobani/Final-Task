c01f3b19746e41c3e5845a6ddec4cb4cb3bebc2f
hassanalnobani1 <hassanalnobani1@gmail.com>
2022-12-24 09:16:05 +0000

    with out add

---
+++ app.js
@@ -0,0 +1,104 @@
+'use strict';
+
+function Job(name, major, imgurl, price) {
+        this.name = name;
+        this.major = major;
+        this.imagePath = imgurl;
+        this.price = price;
+        Jobs.push(this);
+
+    }
+    let Jobs = [];
+    // function Job(name, imgurl, major, price) {
+    // to render the new Job using DOM way
+    Job.prototype.renderJobs = function(){ 
+        
+        // parent div
+        const bigDiv = document.getElementById('Jobs');
+        // child div    
+        const divEle = document.createElement('div');
+        bigDiv.appendChild(divEle);
+
+        const h4 = document.createElement('h4');
+        divEle.appendChild(h4);
+        h4.textContent = this.name;
+        
+        // render the major []
+        const m = document.createElement('m');
+        divEle.appendChild(m);
+            m.textContent = this.major;
+
+    
+        const img = document.createElement('img');
+        divEle.appendChild(img);
+        img.setAttribute('src', this.imagePath);
+
+
+        const p2 = document.createElement('p');
+        divEle.appendChild(p2);
+        p2.textContent = `${this.price} JD`;
+
+        
+
+
+    }
+
+
+
+
+const Plumber = new Job('Ahmed', 'plumber', 'images/plumber.jpg','15');
+const Baker = new Job('Mohammad', 'baker', 'images/baker.png','15');
+const Carpenter = new Job('Khaled', 'carpenter', 'images/carpenter.jpg','15');
+const Smith = new Job('Sameh', 'smith', 'images/smith.jpg','15');
+
+
+for (let j = 0; j < Jobs.length; j++) {
+    Jobs[j].renderJobs();
+
+}
+
+
+let htmlForm = document.getElementById('JobForm');
+htmlForm.addEventListener("submit", addNewJob);
+
+function addNewJob(event) {
+    event.preventDefault();
+    let name = event.target.name.value;
+    let major = event.target.major.value;
+    let imgurl = event.target.imgurl.value;
+    let price = event.target.price.value;
+
+    let newJob = new Job(name,major, imgurl,price);
+    newJob.renderJobs();
+    saveToLocalStorage();
+}
+
+// local storage:
+// 1. where do I need to call this function
+// 2. allDrinks in an array, and setItem methos, takes a string 
+function saveToLocalStorage() {
+    const stringifiedArr = JSON.stringify(Jobs);
+    console.log("Did the array converted to string?", stringifiedArr);
+    localStorage.setItem("Jobs", stringifiedArr);
+
+}
+
+// 1. where do I need to call this function? 
+// 2 . How to dispaly data I get from ls as jobs on my page
+// 3. convert string to array of objects
+// 4. loop through the array to render each object as a job
+function getFromLocalStorage() {
+
+    let jobsStr = localStorage.getItem("Jobs");
+    const JobsArr = JSON.parse(jobsStr);
+    if (JobsArr !== null) {
+        for (let i = Jobs.length; i < JobsArr.length; i++) {
+            const temp = new Job(JobsArr[i].name, JobsArr[i].major, JobsArr[i].imagePath);
+            temp.renderJobs();
+        }
+    }
+
+}
+
+
+getFromLocalStorage();
